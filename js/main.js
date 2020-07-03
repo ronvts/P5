df9ef08bf50d94427b20e59201c10144fdf2e6a2
Ronan Vetois <ronan.vetois@gmail.com>
2020-07-03 14:00:06 +0200

    Suppression message d'erreur si panier vide

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let carts = document.querySelectorAll('.add-cart');
 let nbItems = document.querySelectorAll('.nb-items');
-let panierTemp = JSON.parse(localStorage.getItem("panier"));
 let quantity = 0;
+let panierItem = localStorage.getItem("panier");
+let panierTemp = panierItem == null?[]:JSON.parse(panierItem);
 
 for (line of panierTemp) {
     quantity += Number(line.quantity)
